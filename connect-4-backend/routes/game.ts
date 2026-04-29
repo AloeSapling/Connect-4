64182a3b67f667ae08137c3dc203aef088c1cba2
AloeSapling <aloesapling@gmail.com>
2026-04-29 11:53:42 +0200

    Added validation for the game routes

--- connect-4-backend/routes/game.ts
+++ connect-4-backend/routes/game.ts
@@ -3,21 +3,45 @@ import { Router } from "express";
 import { createLobby, getAllLobbies, getSpecificLobby } from "../database-sqllite/lobby.ts";
 import { CodedError } from "../lib/types.ts";
 import { validateLobbyCode } from "../lib/lib.ts";
+import { Lobby } from "../database-sqllite/models.ts";
 
 const router = Router();
 
 router.get('/create', async (req, res) => {
-	if (req.query.code) {
-		await GameRedis.createGame(req.query.code.toString());
+	try {
+		if (req.query.code) {
+
+			if (!validateLobbyCode(req.query.code.toString()) || await Lobby.findOne({ where: { code: req.query.code } }) === null) {
+				res.status(400).json(new CodedError("BadLobbyCode"));
+				return;
+			}
+			await GameRedis.createGame(req.query.code.toString());
+			res.status(204).send();
+		} else {
+			res.status(400).send(new CodedError("BadLobbyCode"));
+		}
+	} catch {
+		res.status(500).send(new CodedError("ServerError"));
 	}
-	res.send(req.query.code);
 })
 
-router.get('/get', async (req, res) => {
-	if (req.query.code) {
-		const gameState = await GameRedis.getGameState(req.query.code.toString());
-		console.log(gameState);
+router.get('/', async (req, res) => {
+	try {
+		if (req.query.code) {
+
+			if (!validateLobbyCode(req.query.code.toString()) || await Lobby.findOne({ where: { code: req.query.code } }) === null) {
+				res.status(400).json(new CodedError("BadLobbyCode"));
+				return;
+			}
+			const gameState = await GameRedis.getGameState(req.query.code.toString());
+			console.log(gameState);
+			res.status(200).send(JSON.stringify(gameState));
+		} else {
+			res.status(400).send(new CodedError("BadLobbyCode"));
+		}
+	} catch {
+		res.status(500).send(new CodedError("ServerError"));
 	}
-	res.send(`GET: ${req.query.code}`)
 })
+
 export default router;
--- connect-4-backend/routes/lobby.ts
+++ connect-4-backend/routes/lobby.ts
@@ -2,43 +2,44 @@ import { Router } from "express";
 import { createLobby, getAllLobbies, getSpecificLobby } from "../database-sqllite/lobby.ts";
 import { CodedError } from "../lib/types.ts";
 import { validateLobbyCode } from "../lib/lib.ts";
+import { Lobby } from "../database-sqllite/models.ts";
 
 const router = Router();
 
 router.get('/', async (req, res) => {
-  // Search params can include the code (to get a specific lobby) or include filters for the list of lobbies
-  if(req.query.code){
-    // Validate the code
-    if(!validateLobbyCode(req.query.code.toString())){
-      res.status(400).json(new CodedError("BadLobbyCode"));
-    }
+	// Search params can include the code (to get a specific lobby) or include filters for the list of lobbies
+	if (req.query.code) {
+		if (!validateLobbyCode(req.query.code.toString())) {
+			res.status(400).json(new CodedError("BadLobbyCode"));
+			return;
+		}
 
-    try{
-      res.json(await getSpecificLobby(req.query.code.toString()));
-    }catch{
-      res.status(500).json(new CodedError("ServerError"));
-    }
-  }
+		try {
+			res.json(await getSpecificLobby(req.query.code.toString()));
+		} catch {
+			res.status(500).json(new CodedError("ServerError"));
+		}
+	}
 
-  // If no code was provided, return all lobbies
-  else{
-    try{
-      res.json(await getAllLobbies());
-    }catch{
-      res.status(500).json(new CodedError("ServerError"));
-    }
-  }
+	// If no code was provided, return all lobbies
+	else {
+		try {
+			res.json(await getAllLobbies());
+		} catch {
+			res.status(500).json(new CodedError("ServerError"));
+		}
+	}
 })
 
 router.get('/create/', async (req, res) => {
-  // Create a new lobby and return the associated code
-  try{
-    const code  = await createLobby();
-    console.log(code);
-    res.send(code);
-  }catch{
-    res.status(500).json(new CodedError("ServerError"));
-  }
+	// Create a new lobby and return the associated code
+	try {
+		const code = await createLobby();
+		console.log(code);
+		res.send(code);
+	} catch {
+		res.status(500).json(new CodedError("ServerError"));
+	}
 });
 
 export default router;
