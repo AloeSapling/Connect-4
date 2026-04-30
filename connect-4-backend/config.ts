07bd40940d1e5bff25a5b1a5924a033d4904bda0
aloesapling <aloesapling@gmail.com>
2026-04-30 21:10:22 +0200

    Fix: Small code improvements

--- connect-4-backend/config.ts
+++ connect-4-backend/config.ts
@@ -4,3 +4,6 @@ export const GAME_ROWS = 6 as const;
 export const ALL_CODE_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789" as const;
 export const CODE_LENGTH = 8 as const;
 
+export const SERVER_PORT: string = process.env.SERVER_PORT ?? '8080' as const;
+export const CLIENT_URL: string = process.env.CLIENT_URL ?? 'localhost:3000' as const;
+
