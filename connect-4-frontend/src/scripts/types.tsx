4209bdf859ded51ec20a3128cb9c07119af0d8c6
MBXMBX <mwilk958@gmail.com>
2026-04-30 20:28:45 +0200

    feat: add placeholder main site and basic menu/button switching functionality

---
+++ connect-4-frontend/src/scripts/types.tsx
@@ -0,0 +1,11 @@
+export const Menus = {
+TitleScreen: 1,
+GameModeSelect: 2,
+LobbyList: 3,
+GameLobby: 4,
+InGame: 5,
+Settings: 6,
+IDGASAV: 15,
+} as const
+
+export type Menus = typeof Menus[keyof typeof Menus];
