6f502d4b268ffb7a6e708c32233a04bfd170efe6
aloesapling <aloesapling@gmail.com>
2026-05-02 19:14:46 +0200

    Added a websocket example in clientside javascript

--- connect-4-frontend/src/App.tsx
+++ connect-4-frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.css'
+import BackendTest from "./pages/BackendTest.tsx";
 
 // Pages
 const BasePage = lazy(() => import("./pages/BasePage.tsx"));
@@ -11,28 +12,29 @@ const GamePage = lazy(() => import("./pages/GamePage.tsx"));
 const ErrorPage = lazy(() => import("./pages/ErrorPage.tsx"));
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Suspense fallback={<div>Loading...</div>}>
-          {/* All routes are to be defined here so that child routes can also use them */}
-          <Routes>
-            <Route path="/" element={<BasePage />}>
-              {/* Base path */}
-              <Route index element={<HomePage />} />
+	return (
+		<>
+			<BrowserRouter>
+				<Suspense fallback={<div>Loading...</div>}>
+					{/* All routes are to be defined here so that child routes can also use them */}
+					<Routes>
+						<Route path="/" element={<BasePage />}>
+							{/* Base path */}
+							<Route index element={<HomePage />} />
 
-              <Route path="settings" element={<SettingsPage />} />
+							<Route path="settings" element={<SettingsPage />} />
 
-              <Route path="game" element={<GamePage />} />
+							<Route path="game" element={<GamePage />} />
+							<Route path="test" element={<BackendTest />} />
 
-              {/* 404 */}
-              <Route path="*" element={<ErrorPage />} />
-            </Route>
-          </Routes>
-        </Suspense>
-      </BrowserRouter>
-    </>
-  )
+							{/* 404 */}
+							<Route path="*" element={<ErrorPage />} />
+						</Route>
+					</Routes>
+				</Suspense>
+			</BrowserRouter>
+		</>
+	)
 }
 
 export default App
