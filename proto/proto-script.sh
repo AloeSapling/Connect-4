16cc7a2422609ef3c1b09fe7b2fdfa54b074ae79
AloeSapling <aloesapling@gmail.com>
2026-05-08 09:39:01 +0200

    Updated the protobuf generation script

--- proto/proto-script.sh
+++ proto/proto-script.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-npx pbjs -t static-module -w es6 --es6 -p . -o ./proto.js **/*.proto
+npx pbjs -t static-module -w es6 --es6 -p . -o ./proto.js $(find . -name "*.proto")
 npx pbts -o ./proto.d.ts ./proto.js
+
+cp ./proto.d.ts ../connect-4-backend/lib/proto.d.ts
+cp ./proto.js ../connect-4-backend/lib/proto.js
+
+mv ./proto.d.ts ../connect-4-frontend/src/lib/proto.d.ts
+mv ./proto.js ../connect-4-frontend/src/lib/proto.js
