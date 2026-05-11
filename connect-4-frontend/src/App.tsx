3b06e5aded25ef225c6604ef66034637a602b991
AloeSapling <aloesapling@gmail.com>
2026-05-11 21:51:25 +0200

    refactor: removed "Page" from most files in /pages/

--- connect-4-frontend/src/App.tsx
+++ connect-4-frontend/src/App.tsx
@@ -4,116 +4,47 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// Pages
+const BasePage = lazy(() => import("./pages/BasePage.tsx"));
+const Home = lazy(() => import("./pages/Home.tsx"));
+const Settings = lazy(() => import("./pages/Settings.tsx"));
+const Game = lazy(() => import("./pages/Game.tsx"));
+const Error = lazy(() => import("./pages/Error.tsx"));
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      {/* sets menu context for the entire app, thus allowing global usage of `currentMenu` and `setCurrentMenu` */}
+      <MenuProvider value={{ currentMenu, setCurrentMenu }}>
+        <header>
+          <div id='websiteLogo'>
+          </div>
+          <nav>
+            <a href='#'>cool</a>
+            <a href='#'>cooler</a>
+            <a href='#'>coolest</a>
+          </nav>
+        </header>
+      <BrowserRouter>
+        <Suspense fallback={<div>Loading...</div>}>
+          {/* All routes are to be defined here so that child routes can also use them */}
+          <Routes>
+            <Route path="/" element={<BasePage />}>
+              {/* Base path */}
+              <Route index element={<Home />} />
 
-      <div className="ticks"></div>
+              <Route path="settings" element={<Settings />} />
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+              <Route path="game" element={<Game />} />
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+        <footer>
+        </footer>
+      </MenuProvider>
+              {/* 404 */}
+              <Route path="*" element={<Error />} />
+            </Route>
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
     </>
   )
 }
