1c087e353a903775bee7c6ca96529f6aa62d38af
AloeSapling <aloesapling@gmail.com>
2026-05-11 22:25:38 +0200

    merge (to be fixed)

---
+++ connect-4-frontend/src/Components/providers/userProvider.tsx
@@ -0,0 +1,28 @@
+import { getLoggedInUserData } from "@/lib/api";
+import { UserContext } from "@/lib/contexts";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import type { AxiosError } from "axios";
+
+console.log('UserProvider render');
+export function UserProvider({ children }: React.PropsWithChildren) {
+    const { data } = useSuspenseQuery({
+        queryKey: ['user'],
+        queryFn: async () => {
+            try {
+                return await getLoggedInUserData();
+                // eslint-disable-next-line
+            } catch (err: any) {
+                if (err?.response?.status === 401) return null;
+                throw err;
+            }
+        },
+        staleTime: Infinity,
+        retry: (failureCount, error) => (error as AxiosError).status !== 401 && failureCount < 3,
+    })
+
+    return (
+        <UserContext.Provider value={data?.user ?? null}>
+            {children}
+        </UserContext.Provider>
+    )
+}
